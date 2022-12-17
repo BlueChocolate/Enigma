5a90df65b75c06572b058635abc1f2b34c0db54b
Liu <ktche22y@outlook.com>
2022-12-17 12:09:40 +0800

    Create Rotor class

--- EnigmaCore/EnigmaCore.cs
+++ EnigmaCore/EnigmaCore.cs
@@ -1,19 +1,131 @@
-﻿namespace EnigmaCore
+﻿using EnigmaCore;
+using System;
+using System.Dynamic;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
+
+namespace EnigmaCore
 {
     public class Enigma
     {
-        List<Rotor> Rotors = new List<Rotor>();
+        public List<Rotor> Rotors { get; set; } = new List<Rotor>();
 
+
+
+        #region 序列化
+        private static JsonSerializerOptions serializerOptions = new JsonSerializerOptions
+        {
+            // 整齐打印
+            WriteIndented = true,
+            // 设置Json字符串支持的编码，默认情况下，序列化程序会转义所有非 ASCII 字符。 即，会将它们替换为 \uxxxx，其中 xxxx 为字符的 Unicode
+            // 代码。 可以通过设置Encoder来让生成的josn字符串不转义指定的字符集而进行序列化 下面指定了基础拉丁字母和中日韩统一表意文字的基础 Unicode 块（U+4E00-U+9FCC）。 
+            // 基本涵盖了除使用西里尔字母以外所有西方国家的文字和亚洲中日韩越的文字
+            Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.CjkUnifiedIdeographs, UnicodeRanges.CjkSymbolsandPunctuation),
+            // 反序列化不区分大小写
+            PropertyNameCaseInsensitive = true,
+            //// 驼峰命名
+            //PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            //// 对字典的键进行驼峰命名
+            //DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
+            // 忽略只读属性，因为只读属性只能序列化而不能反序列化，所以在以json为储存数据的介质的时候，序列化只读属性意义不大
+            IgnoreReadOnlyProperties = true,
+            // 允许在反序列化的时候原本应为数字的字符串（带引号的数字）转为数字
+            NumberHandling = JsonNumberHandling.AllowReadingFromString
+        };
+
+        public string GetJson()
+        {
+            string jsonString = JsonSerializer.Serialize(this);
+            return jsonString;
+        }
+        public static string GetJsonByEnigma(Enigma enigma)
+        {
+            string jsonString = JsonSerializer.Serialize(enigma, serializerOptions);
+            return jsonString;
+        }
+        public static Enigma GetEnigmaByJson(string json)
+        {
+            var enigma = JsonSerializer.Deserialize<Enigma>(json, serializerOptions);
+            return enigma ?? new Enigma();
+        }
+
+        #endregion
+
+        #region 事件
+        // 事件数据
+        public class LampArgs : EventArgs
+        {
+            public int LampNum { get; set; }
+            public char LampChar { get; set; }
+
+            public LampArgs(int lampNum, char lampChar)
+            {
+                LampNum = lampNum;
+                LampChar = lampChar;
+            }
+        }
+        // 事件
+        public event EventHandler<LampArgs> OnLampChanged = delegate { };
+        #endregion
     }
 
     public class Rotor
     {
+        // 名称
+        public string Name { get; set; }
+        // 接线方式
+        public string Encode { get; set; }
+        // 字母环
+        public int RingSetting { get; set; }
+        // 初始位置
+        public int RotorPosition { get; set; }
+        // 刻痕位置
+        public int[] NotchPosition { get; set; }
 
+        public Rotor(string name = "Default", string encode = "ABCDEFGHIJKLMNOPQRSTUVWXYZ", int ringSetting = 0, int rotorPosition = 0, params int[] notchPosition)
+        {
+            Name = name;
+            Encode = encode;
+            RingSetting= ringSetting;
+            RotorPosition = rotorPosition;
+            NotchPosition = notchPosition;
+        }
+
+        public class LetterPair
+        {
+            public char LeftLetter { get; set; }
+            public char RightLetter { get; set; }
+        }
+
+        public bool IsVerified()
+        {
+            return true;
+        }
+
+        public static class RotorLibrary
+        {
+            public static Rotor RealRotorI = new Rotor("I", "EKMFLGDQVZNTOWYHXUSPAIBRCJ", 0, 0, 16);
+            public static Rotor RealRotorII = new Rotor("II", "AJDKSIRUXBLHWTMCQGZNPYFVOE", 0, 0, 4);
+            public static Rotor RealRotorIII = new Rotor("III", "BDFHJLCPRTXVZNYEIWGAKMUSQO", 0, 0, 21);
+            public static Rotor RealRotorIV = new Rotor("IV", "ESOVPZJAYQUIRHXLNFTGKDCMWB", 0, 0, 9);
+            public static Rotor RealRotorV = new Rotor("V", "VZBRGITYUPSDNHLXAWMJQOFECK", 0, 0, 25);
+            public static Rotor RealRotorVI = new Rotor("VI", "JPGVOUMFYQBENHZRDKASXLICTW", 0, 0, 12, 25);
+            public static Rotor RealRotorVII = new Rotor("VII", "NZJHGRCXMYSWBOUFAIVLPEKQDT", 0, 0, 12, 25);
+            public static Rotor RealRotorVIII = new Rotor("VIII", "FKQHTLXOCBJSPDZRAMEWNIUYGV", 0, 0, 12, 25);
+        }
     }
 
-    public class Reflector { }
+    public class Reflector
+    {
+        // 可以改变连线，也可以转
+    }
 
     public class Plugboard { }
     public class Lampboard { }
-    public class Keyboard { }
+    public class Keyboard
+    {
+        class Stecker { }
+    }
 }
--- EnigmaDemo/Program.cs
+++ EnigmaDemo/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EnigmaCore;
+
 Console.WriteLine("Hello, World!");
+Enigma enigma= new Enigma();
+enigma.Rotors.Add(Rotor.RotorLibrary.RealRotorI);
+enigma.Rotors.Add(Rotor.RotorLibrary.RealRotorII);
+enigma.Rotors.Add(Rotor.RotorLibrary.RealRotorVIII);
+
+Console.WriteLine(Enigma.GetJsonByEnigma(enigma));
